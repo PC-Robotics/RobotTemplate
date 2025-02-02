9d78d871dada9af949e4d405b4b651068c3ba8a3
Dennis Colak <dennis.colak@pinecrest.edu>
2025-02-02 11:11:09 -0500

    added basic teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.support.Robot;
+
+public class MainTeleop extends LinearOpMode {
+    Robot robot = new Robot(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init();
+        robot.imu.resetYaw();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // runs mecanum drive
+            robot.driveBase.mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_bumper);
+
+            robot.driveBase.telemetry();
+            robot.imu.telemetry();
+        }
+    }
+}
